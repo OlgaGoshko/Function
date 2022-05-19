685343863873e96ba96e5d1841a47c431f141905
OlgaGoshko <olga_goshko@ukr.net>
2022-05-19 06:25:39 +0300

    task

--- js/homeWork.js
+++ js/homeWork.js
@@ -1,76 +1,35 @@
- // Телефонна книга, де є можливість:
-// 1. додавати номер телефону
-// 2. змінювати номер телефону
-// 3. додавати номер телефону у чорний список
-// 4. видалити номер телефону
+const listFruits = [
+   { name: 'banan', count: '20', pay: true,},
+   { name: 'orange', count: '15', pay: false,},
+   { name: 'apple', count: '30', pay: false,},
+   { name: 'lemon', count: '45', pay: true,},
+   { name: 'apricot', count: '50', pay: false,},
+]
 
-const phoneNumber = {
-   roman: {
-       tel: '+380952365824',
-       blacklist: false,
-   },
-   kolia: {
-       tel: '+380952365824',
-       blacklist: true,
-   },
-   olia: {
-       tel: '+380952365824',
-       blacklist: false,
-   },
-}
 
-function addNumber(){
-   const name = prompt('Введіть ім*я, під яким бажаєте записати номер: ')
-   const number = prompt('Введіть номер телефону: ')
-   const blackListStatusPrompt = prompt('Чи додати елемент в чорний список (так або ні): ')
-   let blackListStatusValue = false
+function main(){
+   for (let index=0;index<listFruits.length;index++){
+       if(listFruits[index].pay == false){
+           console.log(listFruits[index].name)
+       }
+   }
 
-if (blackListStatusPrompt === 'так'){
-   blackListStatusValue = true
-} 
-
-   phoneNumber[name] = {
-       tel: number,
-       blacklist: blackListStatusValue,
+   for (let index=0;index<listFruits.length;index++){
+       if(listFruits[index].pay == true){
+           console.log(listFruits[index].name)
+       }
    }
 }
+main()
 
-addNumber()
-console.log(phoneNumber)
 
-function setNumber(){
-   const name = prompt('Введіть ім*я, в якому бажаєте змінити номер телефону: ')
-   const number = prompt('Введіть номер телефону: ')
-
-   if (phoneNumber[name]){
-       phoneNumber[name].tel = number
-   } else {
-       console.log('Такого запису не існує')
-   }
+function addFruits(){
+   for (let index=0;index<listFruits.length;index++){
+      if(listFruits[name] == false){
+          console.log(listFruits[name].name)
+      }
+  }
 }
 
-// setNumber()
-// console.log(phoneNumber)
 
-function blackListNumber(){
-   const name = prompt('Введіть ім*я, номер телефону якого бажаєте додати в чорний список: ')
-   const number = prompt('Введіть номер телефону: ')
-   let blackListStatusValue = true
-
-   phoneNumber[name] = {
-      tel: number,
-      blacklist: blackListStatusValue,
-   }
-}
-
-// blackListNumber()
-// console.log(phoneNumber)
-
-function deleteNumber(){
-   const name = prompt('Введіть ім*я, номер телефону який бажаєте видалити: ')
-  
-   delete phoneNumber[name]
-}
-
-// deleteNumber()
-// console.log(phoneNumber)
+console.log(listFruits)
